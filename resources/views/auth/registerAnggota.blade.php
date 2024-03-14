7cd7473e8e8553b0dd38eec50729b132c8fb1580
Vicky <vickynando12@gmal.com>
2024-03-14 20:04:35 +0700

    Merubah tampilan UI Pada Register

--- resources/views/auth/registerAnggota.blade.php
+++ resources/views/auth/registerAnggota.blade.php
@@ -1,45 +1,54 @@
 @extends('app')
 
 @section('container')
-<form action="/register/anggota" method="POST">
-    @csrf
-    <h2>Data Anggota</h2>
-    <div>
-        <label for="nama_anggota">Nama Anggota</label>
-        <input type="text" name="nama_anggota" id="nama_anggota">
-    </div>
-    <div>
-        <label for="email">email</label>
-        <input type="text" name="email" id="email">
-    </div>
-    <div>
-        <label for="password">Pasword</label>
-        <input type="password" name="password" id="password">
-    </div>
-    <div>
-        <label for="token">Token</label>
-        <input type="text" name="token" id="token">
-        @if ($errors->has('token'))
-            <span class="text-danger">{{ $errors->first('token') }}</span>
-        @endif
-        @if ($errors->has('limit'))
-        <span class="text-danger">{{ $errors->first('limit') }}</span>
-    @endif
-    </div>
-    <div class="hidden">
-        <input type="level" name="level" id="level" value="peserta">
-    </div>
-    <div class="hidden">
-        <input type="status" name="status" id="status" value="anggota">
-    </div>
-    <div>
-    <div>
-        <button type="submit">Create an account</button>
-    </div>
-</form>
+<div class="bg-left-top fixed top-0 left-20">
+    <img src="{{ asset('assets/img/Group 634.png') }}" alt="Rocket" class="block w-full h-full object-contain">
+</div>
 
-<p>
-    Already have an account?
-    <a href="/" method="get">Login here</a>
-</p>
+<div class="bg-green-50 min-h-screen flex items-center justify-end" style="margin-right: 4cm;">
+    <div class="max-w-md w-full p-10 rounded-lg shadow-lg mx-auto lg:mx-0">
+        <h2 class="text-2xl font-semibold text-gray-800 mb-1 text-center">Data Anggota</h2> <br>
+
+        <form action="/register/anggota" method="POST">
+            @csrf
+            <div class="mb-4">
+                <label for="nama_anggota" class="block font-medium text-gray-700">Nama Anggota</label>
+                <input type="text" name="nama_anggota" id="nama_anggota" class="w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-80 ">
+            </div>
+
+            <div class="mb-4">
+                <label for="email" class="block font-medium text-gray-700">Email</label>
+                <input type="email" name="email" id="email" class="w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-80 ">
+            </div>
+
+            <div class="mb-4">
+                <label for="password" class="block font-medium text-gray-700">Password</label>
+                <input type="password" name="password" id="password" class="w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-80 ">
+            </div>
+
+            <div class="mb-4">
+                <label for="token" class="block font-medium text-gray-700">Token</label>
+                <input type="text" name="token" id="token" class="w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-80 ">
+                @if ($errors->has('token'))
+                    <span class="text-danger">{{ $errors->first('token') }}</span>
+                @endif
+                @if ($errors->has('limit'))
+                    <span class="text-danger">{{ $errors->first('limit') }}</span>
+                @endif
+            </div>
+
+            <input type="hidden" name="level" id="level" value="peserta">
+            <input type="hidden" name="status" id="status" value="anggota">
+
+            <div class="mb-4">
+                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Create an account</button>
+            </div>
+        </form>
+
+        <p class="text-center">
+            Already have an account?
+            <a href="/" class="text-blue-500 hover:text-blue-700">Login here</a>
+        </p>
+    </div>
+</div>
 @endsection
--- resources/views/auth/registerKetua.blade.php
+++ resources/views/auth/registerKetua.blade.php
@@ -1,30 +1,42 @@
 @extends('app')
 
 @section('container')
-    <form action="/register/ketua" method="POST">
-        @csrf
-        <h2>Data Tim</h2>
-        <div>
-            <label for="nama_tim">Nama Tim</label>
-            <input type="text" name="nama_tim" id="nama_tim">
-        </div>
-        <div>
-            <label for="institusi">Institusi</label>
-            <input type="text" name="institusi" id="institusi">
-        </div>
-        <h2>Data Ketua</h2>
-        <div>
-            <label for="nama_ketua">Nama Ketua</label>
-            <input type="text" name="nama_ketua" id="nama_ketua">
-        </div>
-        <div>
-            <label for="email">email</label>
-            <input type="text" name="email" id="email">
-        </div>
-        <div>
-            <label for="password">Pasword</label>
-            <input type="password" name="password" id="password">
-        </div>
+   
+<div class="bg-left-top fixed top-0 left-20">
+    <img src="{{ asset('assets/img/Group 634.png') }}" alt="Rocket" class="block w-full h-full object-contain">
+</div>
+
+<div class="bg-green-50 min-h-screen flex items-center justify-end" style="margin-right: 4cm;">
+    <div class="max-w-md w-full p-10 rounded-lg shadow-lg mx-auto lg:mx-0">
+        <h2 class="text-2xl font-semibold text-gray-800 mb-1 text-center">Data Anggota</h2> <br>
+
+        <form action="/register/anggota" method="POST">
+            @csrf
+            <div class="mb-4">
+                <label for="nama_tim" class="block font-medium text-gray-700">Nama Tim</label>
+                <input type="text" name="nama_tim" id="nama_tim" class="w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-80 ">
+            </div>
+
+            <div class="mb-4">
+                <label for="institusi" class="block font-medium text-gray-700">Institusi</label>
+                <input type="text" name="institusi" id="institusi" class="w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-80 ">
+            </div>
+
+            <div class="mb-4">
+                <label for="nama_ketua" class="block font-medium text-gray-700">Nama Ketua</label>
+                <input type="text" name="nama_ketua" id="nama_ketua" class="w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-80 ">
+            </div>
+
+            <div class="mb-4">
+                <label for="email" class="block font-medium text-gray-700">Email</label>
+                <input type="text" name="email" id="email" class="w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-80 ">
+            </div>
+
+            <div class="mb-4">
+                <label for="password" class="block font-medium text-gray-700">Password</label>
+                <input type="password" name="password" id="token" class="w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-80 ">
+            </div>
+            
         <div class="hidden">
             <input type="level" name="level" id="level" value="peserta">
         </div>
@@ -37,6 +49,6 @@
     </form>
     <p>
         Already have an account?
-        <a href="/" method="get">Login here</a>
+        <a href="/" method="get">  Login here</a>
     </p>
 @endsection
