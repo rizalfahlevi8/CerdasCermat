3bfa5f2f3ea2c377f875d9a4061d0d782d9c76d3
Rizal Fahlevi <rizalfahlevi810@gmail.com>
2024-03-20 11:33:58 +0700

    menambahkan dashboard

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -8,7 +8,7 @@
         @auth
         <button type="button" class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
             <span class="sr-only">Open user menu</span>
-            <img class="w-8 h-8 rounded-full" src="{{ asset('assets/img/User/' . auth()->user()->foto) }}" alt="user photo">
+            <img class="w-8 h-8 rounded-full" src="{{ asset('assets/foto/maleDefault.jpeg') }}" alt="user photo">
         </button>
         <!-- Dropdown menu -->
         <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600 " id="user-dropdown">
